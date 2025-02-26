33f22194b6008f8e1c1a3799da8ee9e021902870
NISHIKANT <nishikantraj1977@gmail.com>
2025-02-26 18:55:39 +0530

    Skeleton bug fixed

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -35,13 +35,20 @@ const Leaderboard = () => {
 
   if(isLoading)
     return (
-      <div className="flex items-center space-x-4">
-      <Skeleton className="h-12 w-12 rounded-full" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+      <div className="flex justify-center items-center">
+        <div className="flex items-center space-x-4">
+          <Skeleton className="h-12 w-12 rounded-full bg-cyan-300" />
+          <div className="space-y-2 bg-[#ABB3C6] flex justify-center items-center">
+            <div className="flex justify-center items-center">
+              <p className="text-center px-5 text-2xl">Data fetching...</p>
+            </div>
+            <div>
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
     )
 
     if(error)
