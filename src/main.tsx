68e6e80efc1c3490c81928fff6d0048e52df78aa
NISHIKANT <nishikantraj1977@gmail.com>
2025-03-02 00:27:47 +0530

    Signin and Signup page integrated with backend

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,24 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            style: {
+              background: "#161B22",
+              color: "#E3E6E8",
+              border: "1px solid #3DDC97",
+            },
+            success: {
+              icon: "✅",
+              style: { background: "#3DDC97", color: "black" },
+            },
+            error: {
+              icon: "❌",
+              style: { background: "#FF6969", color: "white" },
+            },
+          }}
+        />
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
---
+++ src/utils/config.ts
@@ -0,0 +1,3 @@
+const BASE_API_URL = "https://codetrackbackend.onrender.com/api";
+
+export default BASE_API_URL;
