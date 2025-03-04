94c04c9db89a81a885726b4fd43a6fb66f26738c
NISHIKANT <nishikantraj1977@gmail.com>
2025-03-04 21:32:08 +0530

    Responsive design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,17 +2,16 @@ import { PropsWithChildren } from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 
-
 const Layout = ({children}:PropsWithChildren) => {
   return (
-    <div className="px-15 py-2">
+    <div className="w-full min-h-screen flex flex-col">
         <Header/>
 
-        <main>
+        <main className="flex-grow px-4 sm:px-6 md:px-15 py-2">
             {children}
         </main>
 
-        <footer className="mt-30">
+        <footer className="w-full">
           <Footer/>
         </footer>
     </div>
