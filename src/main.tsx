3355453557ac434058fbf072e89814fd66d20afd
NISHIKANT <nishikantraj1977@gmail.com>
2025-03-06 18:40:51 +0530

    Google analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,9 @@ import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-
 const queryClient = new QueryClient();
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
